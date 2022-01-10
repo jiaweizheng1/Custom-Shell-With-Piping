3d19f97508af15041930ae6fe6f7403630e6d435
jiaweizheng <jiaweizheng416528@gmail.com>
2022-01-09 18:00:52 -0800

    Co-authored-by: Mbai2000 <Mbai2000@users.noreply.github.com>

--- sshell.c
+++ sshell.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define CMDLINE_MAX 512
 
@@ -14,38 +15,88 @@ int main(void)
                 char *nl;
                 int retval;
 
-                /* Print prompt */
+                // Print prompt
                 printf("sshell$ ");
                 fflush(stdout);
 
-                /* Get command line */
+                // Get command line
                 fgets(cmd, CMDLINE_MAX, stdin);
 
-                /* Print command line if stdin is not provided by terminal */
+                // Print command line if stdin is not provided by terminal
                 if (!isatty(STDIN_FILENO))
                 {
                         printf("%s", cmd);
                         fflush(stdout);
                 }
 
-                /* Remove trailing newline from command line */
+                // Remove trailing newline from command line
                 nl = strchr(cmd, '\n');
                 if (nl)
                         *nl = '\0';
 
-                /* Builtin command */
+                // Builtin command
                 if (!strcmp(cmd, "exit"))
                 {
                         fprintf(stderr, "Bye...\n");
                         break;
                 }
 
-                /* Regular command */
-                retval = system(cmd);
+                // Regular command
+                pid_t pid;
+                char *args[16] = {}; //specifications said maximum of 16 arguments so we can limit size of args array to save memory
+                char *exec_program;  //initialize empty char for program to be executed
 
-                fprintf(stderr, "Return status value for '%s': %d\n",
+                //beginning of string parsing
+                char cmd_cpy[CMDLINE_MAX];
+
+                strncpy(cmd_cpy, cmd, sizeof(cmd));
+
+                char delimiter[] = " ";
+                char *ptr = strtok(cmd_cpy, delimiter);
+                int i = 0;
+
+                while (ptr != NULL)
+                {
+                        args[i] = ptr;
+                        ptr = strtok(NULL, delimiter);
+                        i++;
+                }
+                exec_program = args[0];
+                //end of string parsing
+
+                pid = fork(); // fork, creating child process
+                if (pid == 0) // Child
+                {
+                        execvp(exec_program, args); //execvp so shell automatically search programs in $Path
+                        exit(0);
+                }
+                else if (pid != 0) // Parent
+                {
+                        int status;
+                        waitpid(pid, &status, 0);     // wait for child to finishing executing, then continue
+                        retval = WEXITSTATUS(status); // WEXITSTATUS gets exit status of child and puts into retval
+                }
+
+                fprintf(stderr, "Return status value for '%s': %d\n", // prints exit status of child
                         cmd, retval);
         }
 
         return EXIT_SUCCESS;
 }
+
+/*int cd(char *path)
+{
+        if sizeof(argv) != 2 {
+                
+        }
+                printf()
+        }
+chdir(path);
+}
+*/
+
+/*struct cmd{
+char command[50];
+cha
+}
+*/
